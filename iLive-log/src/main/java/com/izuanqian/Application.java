7292bb9f4a2aaef42c460e8446c68df642af17fd
sanlion <sanlion@vip.qq.com>
2017-02-23 15:18:30 +0800

    heiheihei

--- iLive-log/src/main/java/com/izuanqian/Application.java
+++ iLive-log/src/main/java/com/izuanqian/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		// 嘿嘿嘿...
 		SpringApplication.run(Application.class, args);
 	}
 }
